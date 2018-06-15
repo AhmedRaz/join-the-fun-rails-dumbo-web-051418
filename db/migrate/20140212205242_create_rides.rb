5884587d992f8e915435d0663f596a13689f48d2
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-15 15:40:19 -0400

    Completed join-the-fun-rails-dumbo-web-051418

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.integer :taxi_id
+      t.integer :passanger_id
       t.timestamps null: false
     end
   end
---
+++ db/migrate/20180615193818_rename_column_in_passengers.rb
@@ -0,0 +1,5 @@
+class RenameColumnInPassengers < ActiveRecord::Migration
+  def change
+    rename_column :rides, :passanger_id, :passenger_id
+  end
+end
